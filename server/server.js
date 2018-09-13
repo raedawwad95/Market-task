b18e8fe8d8cf6e86699d4669b6adb8595550fcdd
raedawwad95 <awwad12121@gmail.com>
2018-09-14 01:19:59 +0300

    router for projects

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var Employees = require('./resources/Employees/EmployeeRouter')
-var Equipment = require('./resources/Equipments/EquipmentRouter')
+var Employees = require('./resources/Employees/EmployeeRouter');
+var Equipment = require('./resources/Equipments/EquipmentRouter');
+var Project = require('./resources/Projects/ProjectRouter');
 
 var db = require ('./db');
 
@@ -11,7 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/employee',Employees);
-app.use('/equipment',Equipment)
+app.use('/equipment',Equipment);
+app.use('/project',Project);
 app.get('/', (req, res) => res.send('Hello World!'))
 
 module.exports= app;
