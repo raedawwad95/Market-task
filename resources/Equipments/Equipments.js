712c3ac8f3b27cae21960e9e2e8763ec9d1bdbd3
raedawwad95 <awwad12121@gmail.com>
2018-09-13 20:36:48 +0300

    create equipments schema

---
+++ resources/Equipments/Equipments.js
@@ -0,0 +1,25 @@
+var mongoose =require('mongoose');
+var db=require('../../db');
+
+//Equipment schema 
+
+var EquipmentSchema = mongoose.Schema({
+	name :{
+		type: String,
+		required:true,
+		unique:true,
+	},
+	serialNumber :{
+		type :Number,
+		required true,
+	},
+	image: {
+		type :String,
+		required true,
+	}
+})
+
+
+var Equipment =mongoose.model('Equipment',Equipmentschema);
+
+module.exports=Equipment;
