1e3f6de6f0f16b4969590ea782b48dfbda795380
raedawwad95 <awwad12121@gmail.com>
2018-09-13 20:52:10 +0300

    connect with employeesRouter

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var Employees = require('../resources/Employees/EmployeeRouter.js')
+
 var db = require ('./db');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('empl',Employees)
 app.get('/', (req, res) => res.send('Hello World!'))
 
 module.exports= app;
