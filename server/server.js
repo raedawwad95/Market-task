7b23c8c1769c3221fbf704bdb707d09ccd323fe2
raedawwad95 <awwad12121@gmail.com>
2018-09-13 20:15:29 +0300

    connect db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require ('./db');
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
