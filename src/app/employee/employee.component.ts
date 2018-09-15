f1d552b9868d8d8bafd7aab6d6e7286d92404ace
raedawwad95 <awwad12121@gmail.com>
2018-09-15 15:54:45 +0300

    create employees component and retreive and the data

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from "../service.service";
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  employees: any = [];
+  constructor(private _service: ServiceService) { }
+
+  ngOnInit() {
+  	this._service.getEmployeesData().subscribe(employee => {
+        this.employees = employee;
+        
+  });
+  }
+
+}
