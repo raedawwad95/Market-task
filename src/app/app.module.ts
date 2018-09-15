7401eec97b9ec57bd2d47d9ea07974df0507d307
raedawwad95 <awwad12121@gmail.com>
2018-09-15 15:53:04 +0300

    import http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {MatGridListModule} from '@angular/material';
+import { ServiceService } from './service.service';
 import { EquipmentComponent } from './equipment/equipment.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ProjectComponent } from './project/project.component';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EquipmentComponent
+    EquipmentComponent,
+    EmployeeComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
+    MatGridListModule,
     RouterModule.forRoot([
-      { path: 'equipment', component: EquipmentComponent }    ])
+      { path: 'equipment', component: EquipmentComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'project', component: ProjectComponent }       
+      ])
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
