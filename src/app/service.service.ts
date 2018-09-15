73f9fd5d8830ebf370275bb93d702de32b53b5de
raedawwad95 <awwad12121@gmail.com>
2018-09-15 15:52:24 +0300

    get employee data

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 
-@Injectable()
+
+var serverApi= 'http://localhost:3000';
+
+@Injectable({
+	providedIn: 'root'
+})
 
 
 export class ServiceService {
+constructor(private http: Http,private httpClient: HttpClient) { }
 
+getEmployeesData() {
+    return this.http.get(`${serverApi}/employee/retrieve/`).pipe(map(res => res.json()));
+  }
 }
