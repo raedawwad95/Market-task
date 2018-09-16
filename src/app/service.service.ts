331bd29115aa941fa5b3756366b6cb978066f65d
raedawwad95 <awwad12121@gmail.com>
2018-09-16 07:05:46 +0300

    add , retrieve Projects Data

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -27,15 +27,15 @@ getEmployeesData() {
 
   this.http.post(`${serverApi}/employee/add/`, {name:name,nationality:nationality,jobTitle:jobTitle}).subscribe(res => console.log(res.json()));
   }
-  
+
   editEmployee(name:any,jobTitle:any){
   	this.http.put(`${serverApi}/employee/edit/`, {name:name,jobTitle:jobTitle}).subscribe(res => console.log(res.json()));
   }
+
+
   getEquipmentsData() {
     return this.http.get(`${serverApi}/equipment/retrieve/`).pipe(map(res => res.json()));
   }
-
-  
   deleteEquipment(equi: any) {
     
     return this.http.delete(`${serverApi}/equipment/${equi}`).subscribe(res => console.log(res.json()));
@@ -46,5 +46,13 @@ getEmployeesData() {
   }
   editEquipment(name:any,serialNumber:any,image:any){
     this.http.put(`${serverApi}/equipment/edit/`, {name:name,serialNumber:serialNumber,image:image}).subscribe(res => console.log(res.json()));
+  }
+  addProject(name:any , from:any,to:any){
+    this.http.post(`${serverApi}/project/add`,{name:name,from:from,to:to}).subscribe(res=>console.log(res.json()));
+  }
+
+
+  getProjectsData() {
+    return this.http.get(`${serverApi}/project/retrieve/`).pipe(map(res => res.json()));
   }
 }
