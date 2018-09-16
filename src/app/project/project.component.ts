0122bee432f1362a76a8289c5098806fc95945fa
raedawwad95 <awwad12121@gmail.com>
2018-09-16 07:06:16 +0300

    create add project daata

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ServiceService} from "../service.service"
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
-  constructor() { }
+  name ="";
+  from ="";
+  to ="";
+  display ="none";
+  projects :any =[];
+  constructor(private _service: ServiceService) { }
 
   ngOnInit() {
+  	this._service.getProjectsData().subscribe(project => {
+        this.projects = project;
+        
+  });
   }
-
+  addProject(){
+  	this.display="block";
+  	console.log(this.projects)
+  }
+  onChangeName(name){
+    this.name=name;
+  }
+  onChangeFrom(from1){
+    this.from=from1;
+  }
+  onChangeTo(to){
+    this.to=to;
+  }
+  saveData(){
+   this._service.addProject(this.name,this.from,this.to)
+    this.display='none';
+   
+    
+  }
+  onCloseHandled(){
+       this.display='none'; 
+    }
 }
