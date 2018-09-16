0a8d09ebd0b2327651e3a01c93dff135f4e1b9ef
raedawwad95 <awwad12121@gmail.com>
2018-09-16 15:09:08 +0300

    Upload Image

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from "../service.service";
+import $ from 'jquery';
 
 @Component({
   selector: 'app-equipment',
@@ -15,6 +16,9 @@ export class EquipmentComponent implements OnInit {
   serialEdit=0;
   imageEdit ="";
   nameEdit ="";
+  displayImage= 'none';
+  displayImageEdit= 'none';
+  waitToUp='block';
   equipments: any = [];
   constructor(private _service: ServiceService) { }
 
@@ -24,7 +28,35 @@ export class EquipmentComponent implements OnInit {
         
   });
   }
-
+  onChangeImage(image){
+  var imgReader = new FileReader();
+  var img = image.target.files[0];
+  var that = this;
+  var imgCode = ''
+  var target = image.target.name;
+  imgReader.readAsDataURL(img)
+  imgReader.onload = function(upload) {
+    imgCode = upload.target.result
+    imgCode = imgCode.slice(22)
+    $.ajax({
+      url: `https://api.imgur.com/3/image`,
+      method: 'POST',
+      headers: {"Authorization": "Client-ID 307e8453e9ee1b3"},
+      data:imgCode
+    })
+    .done (function (data) {
+        
+        that.image= data.data.link;
+        that.displayImage= 'block';
+        that.waitToUp='none';
+      
+    })
+    .fail(function( jqXHR, textStatus ) {
+      alert("item not found, textStatus");
+    });
+  };
+  
+}
   delete( equi:any){
     this._service.deleteEquipment(equi);
     location.reload();
@@ -55,13 +87,37 @@ export class EquipmentComponent implements OnInit {
   onChangeSerial(serialNumber){
     this.serialNumber=serialNumber;
   }
-  onChangeImage(image){
-    this.image=image;
-  }
+
   onChangeImageEdit(image){
-    this.imageEdit=image;
+    var imgReader = new FileReader();
+  var img = image.target.files[0];
+  var that = this;
+  var imgCode = ''
+  var target = image.target.name;
+  imgReader.readAsDataURL(img)
+  imgReader.onload = function(upload) {
+    imgCode = upload.target.result
+    imgCode = imgCode.slice(22)
+    $.ajax({
+      url: `https://api.imgur.com/3/image`,
+      method: 'POST',
+      headers: {"Authorization": "Client-ID 307e8453e9ee1b3"},
+      data:imgCode
+    })
+    .done (function (data) {
+        
+        that.imageEdit= data.data.link;
+        that.displayImageEdit= 'block';
+        that.waitToUp ='none';
+      
+    })
+    .fail(function( jqXHR, textStatus ) {
+      alert("item not found, textStatus");
+    });
+  };
   }
   saveData(){
+    console.log(this.image)
    this._service.addEquipment(this.name,this.serialNumber,this.image)
     this.display='none';
     location.reload();
