7055e9a2614756179fa43a045808d7cc10671ca6
raedawwad95 <awwad12121@gmail.com>
2018-09-16 07:22:57 +0300

    change the add to onther component

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -6,39 +6,14 @@ import {ServiceService} from "../service.service"
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-  name ="";
-  from ="";
-  to ="";
-  display ="none";
+  
   projects :any =[];
   constructor(private _service: ServiceService) { }
 
   ngOnInit() {
   	this._service.getProjectsData().subscribe(project => {
-        this.projects = project;
-        
+        this.projects = project; 
   });
   }
-  addProject(){
-  	this.display="block";
-  	console.log(this.projects)
-  }
-  onChangeName(name){
-    this.name=name;
-  }
-  onChangeFrom(from1){
-    this.from=from1;
-  }
-  onChangeTo(to){
-    this.to=to;
-  }
-  saveData(){
-   this._service.addProject(this.name,this.from,this.to)
-    this.display='none';
-   
-    
-  }
-  onCloseHandled(){
-       this.display='none'; 
-    }
+
 }
